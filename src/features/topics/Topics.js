2602fc36006b90f8bf46765a1bf8e7f78f678212
merv-e <34655454+merv-e@users.noreply.github.com>
2024-01-17 15:45:48 +0100

    .included in the previous commit.

--- src/features/topics/Topics.js
+++ src/features/topics/Topics.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import ROUTES from "../../app/routes";
-// import selector
+import topicsReducer, { topicsSlice, topics, selectTopics } from "./topicsSlice";
+import { useSelector } from "react-redux";
 
 export default function Topics() {
-  const topics = {}; // replace this with a call to your selector to select all the topics in state
+  const topics = useSelector(selectTopics);
+  // console.log(topics);  
+
+  // const x = Object.values(topics).map((topic) => topic);
+  // console.log(x);
 
   return (
     <section className="center">
@@ -12,15 +17,16 @@ export default function Topics() {
       <ul className="topics-list">
         {Object.values(topics).map((topic) => (
           <li className="topic" key={topic.id}>
-          <Link to={ROUTES.topicRoute(topic.id)} className="topic-link">
-           <div className="topic-container">
-             <img src={topic.icon} alt="" />
-             <div className="text-content">
-               <h2>{topic.name}</h2>
-               <p>{topic.quizIds.length} Quizzes</p>
-             </div>
-           </div>
-         </Link>
+            <Link to={ROUTES.topicRoute(topic.id)} className="topic-link">
+              <div className="topic-container">
+                <img src={topic.icon} alt="" />
+                <div className="text-content">
+                  <h2>{topic.name}</h2>
+                  <p>{topic.quizIds.length} Quizzes</p> 
+                
+                </div>
+              </div>
+            </Link>
           </li>
         ))}
       </ul>
