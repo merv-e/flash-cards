51a9fde6de2a494149f1577af888599f89b8f8b2
merv-e <34655454+merv-e@users.noreply.github.com>
2024-04-28 20:34:14 +0300

    the error is fixed.

--- src/features/topics/Topic.js
+++ src/features/topics/Topic.js
@@ -5,7 +5,7 @@ import ROUTES from "../../app/routes";
 
 export default function Topic() {
   const topics = useSelector((state) => state.topics.topics);  
-  const quizzes = {}; // replace with selector
+  const quizzes = useSelector(state => state.quizzes.quizzes);
   const { topicId } = useParams();
   const topic = topics[topicId];
 
